1f74d8121b7e3854ad0e0876c12373d5427cd327
anarod13 <anam.rodriguez13@gmail.com>
2023-03-23 12:48:57 -0300

    Refactor horizon servers as credentials

--- credentials/StellarApi.credentials.ts
+++ credentials/StellarApi.credentials.ts
@@ -1,14 +1,47 @@
 import { ICredentialType, INodeProperties } from 'n8n-workflow';
 
 export class StellarApi implements ICredentialType {
-	name = 'stellarCredentialApi';
-	displayName = 'Stellar Credential API';
+	name = 'stellarNetworkApi';
+	displayName = 'Stellar Network API';
+	description = 'Youre operating in this network';
 	properties: INodeProperties[] = [
 		{
-			displayName: 'User Name',
-			name: 'username',
-			type: 'string',
+			displayName: 'Stellar Network',
+			name: 'network',
+			type: 'options',
 			default: '',
+			options: [
+				{ name: 'Testnet', value: 'testnet' },
+				{ name: 'Pubnet', value: 'pubnet' },
+				{ name: 'Futurenet', value: 'futurenet' },
+				{ name: 'Custom', value: 'custom' },
+			],
+		},
+		{
+			displayName: 'Horizon URL',
+			name: 'networkUrl',
+			type: 'string',
+			required: true,
+			displayOptions: {
+				show: {
+					network: ['custom'],
+				},
+			},
+			default: '',
+			placeholder: 'https://horizon-testnet.stellar.org',
+		},
+		{
+			displayName: 'Network Passphrase',
+			name: 'networkPassphrase',
+			type: 'string',
+			required: true,
+			displayOptions: {
+				show: {
+					network: ['custom'],
+				},
+			},
+			default: '',
+			placeholder: 'Test SDF Network ; September 2015',
 		},
 	];
 }
