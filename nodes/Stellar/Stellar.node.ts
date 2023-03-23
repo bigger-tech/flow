4e11b32aba3f35f9a8e6f68c0692a81f89144075
Ja-boop <56199168+Ja-boop@users.noreply.github.com>
2023-03-23 11:33:08 -0300

    Add a swap assets operation for the Stellar node

--- nodes/Stellar/Stellar.node.ts
+++ nodes/Stellar/Stellar.node.ts
@@ -4,7 +4,11 @@ import {
 	INodeType,
 	INodeTypeDescription,
 } from 'n8n-workflow';
-import { fundAccount, createAccountKeypair, getLastPayment } from './services/stellar';
+
+import { Asset } from 'stellar-sdk';
+
+import IAsset from './entities/IAsset';
+import { fundAccount, createAccountKeypair, getLastPayment, swapAssets } from './services/stellar';
 
 export class Stellar implements INodeType {
 	description: INodeTypeDescription = {
@@ -56,6 +60,11 @@ export class Stellar implements INodeType {
 						value: 'getPayment',
 						action: 'Get last payment',
 					},
+					{
+						name: 'Swap on DEX',
+						value: 'swapAssets',
+						action: 'Swap assets',
+					},
 				],
 				noDataExpression: true,
 				default: 'createAccount',
@@ -67,7 +76,7 @@ export class Stellar implements INodeType {
 				required: true,
 				displayOptions: {
 					show: {
-						operation: ['getPayment'],
+						operation: ['getPayment', 'swapAssets'],
 						resource: ['stellar'],
 					},
 				},
@@ -75,8 +84,125 @@ export class Stellar implements INodeType {
 				placeholder: '1234',
 				description: 'Account public key',
 			},
+			{
+				displayName: 'Amount',
+				name: 'amount',
+				type: 'string',
+				required: true,
+				displayOptions: {
+					show: {
+						operation: ['swapAssets'],
+						resource: ['stellar'],
+					},
+				},
+				default: '',
+				placeholder: '100',
+			},
+			{
+				displayName: 'Slippage',
+				name: 'slippage',
+				type: 'collection',
+				placeholder: 'Select slippage tolerance',
+				required: true,
+				default: {},
+				options: [
+					{
+						displayName: 'Amount',
+						name: 'amount',
+						type: 'options',
+						options: [
+							{
+								name: '0.1%',
+								value: '0.1',
+							},
+							{
+								name: '1.0%',
+								value: '1.0',
+							},
+							{
+								name: '2.0%',
+								value: '2.0',
+							},
+						],
+						default: '0.1',
+					},
+				],
+				displayOptions: {
+					show: {
+						resource: ['stellar'],
+						operation: ['swapAssets'],
+					},
+				},
+			},
+			{
+				displayName: 'Source Asset',
+				name: 'sourceAsset',
+				type: 'fixedCollection',
+				required: true,
+				options: [
+					{
+						name: 'values',
+						displayName: 'Values',
+						values: [
+							{
+								displayName: 'Code',
+								name: 'code',
+								type: 'string',
+								default: 'native',
+							},
+							{
+								displayName: 'Issuer',
+								name: 'issuer',
+								type: 'string',
+								default: 'native',
+							},
+						],
+					},
+				],
+				displayOptions: {
+					show: {
+						operation: ['swapAssets'],
+						resource: ['stellar'],
+					},
+				},
+				default: {},
+			},
+			{
+				displayName: 'Destination Asset',
+				name: 'destinationAsset',
+				type: 'fixedCollection',
+				required: true,
+				options: [
+					{
+						name: 'values',
+						displayName: 'Values',
+						values: [
+							{
+								displayName: 'Code',
+								name: 'code',
+								type: 'string',
+								default: 'native',
+							},
+							{
+								displayName: 'Issuer',
+								name: 'issuer',
+								type: 'string',
+								default: 'native',
+							},
+						],
+					},
+				],
+				displayOptions: {
+					show: {
+						operation: ['swapAssets'],
+						resource: ['stellar'],
+					},
+				},
+				default: {},
+			},
 		],
 	};
+
 	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
 		const items = this.getInputData();
 		const operation = this.getNodeParameter('operation', 0) as string;
@@ -100,8 +226,45 @@ export class Stellar implements INodeType {
 				const publicKeyParam = this.getNodeParameter('publicKey', 1) as string;
 				const lastPayment = await getLastPayment(publicKeyParam);
 				outputData.push(lastPayment);
+			case 'swapAssets':
+				try {
+					let sourceAsset: Asset;
+					let destinationAssets: Asset;
+					const amount = this.getNodeParameter('amount', 1) as string;
+					const publicKeyParam = this.getNodeParameter('publicKey', 1) as string;
+					const slippageAmount = this.getNodeParameter('slippage', 1) as { amount: string };
+					const sourceAssetValues = this.getNodeParameter('sourceAsset', 1) as IAsset;
+					const destinationAssetValues = this.getNodeParameter('destinationAsset', 1) as IAsset;
+
+					if (sourceAssetValues.values.code === 'native') {
+						sourceAsset = Asset.native();
+					} else {
+						sourceAsset = new Asset(sourceAssetValues.values.code, sourceAssetValues.values.issuer);
+					}
+
+					if (destinationAssetValues.values.code === 'native') {
+						destinationAssets = Asset.native();
+					} else {
+						destinationAssets = new Asset(
+							destinationAssetValues.values.code,
+							destinationAssetValues.values.issuer,
+						);
+					}
+
+					const swapResult = await swapAssets(
+						sourceAsset,
+						destinationAssets,
+						amount,
+						publicKeyParam,
+						slippageAmount.amount,
+					);
+					outputData.push(swapResult);
+				} catch (e) {
+					outputData.push(e.message);
+				}
 				break;
 		}
+
 		return [this.helpers.returnJsonArray(outputData)];
 	}
 }
--- nodes/Stellar/services/stellar.ts
+++ nodes/Stellar/services/stellar.ts
@@ -1,12 +1,22 @@
 import { IHttpRequestOptions } from 'n8n-workflow';
-import StellarSdk, { StrKey } from 'stellar-sdk';
+import {
+	Asset,
+	StrKey,
+	Keypair,
+	Server,
+	Operation,
+	TransactionBuilder,
+	BASE_FEE,
+	Networks,
+} from 'stellar-sdk';
+
 import InvalidPublicKeyError from './error/InvalidPublicKeyError';
 
 const FRIENDBOT_URL = 'https://friendbot.stellar.org/?addr=';
 const stellarServer = 'https://horizon-testnet.stellar.org';
 
 export function createAccountKeypair() {
-	const pair = StellarSdk.Keypair.random();
+	const pair = Keypair.random();
 	const newKeypair = {
 		publicKey: pair.publicKey(),
 		secretKey: pair.secret(),
@@ -27,9 +37,69 @@ export function fundAccount(publicKey: string): IHttpRequestOptions {
 }
 
 export async function getLastPayment(publicKey: string) {
-	const server = new StellarSdk.Server(stellarServer);
+	const server = new Server(stellarServer);
 	const payments = await server.payments().forAccount(publicKey).call();
 	const lastPayment = payments.records[payments.records.length - 1];
 
 	return lastPayment;
+}
+
+export async function swapAssets(
+	sourceAsset: Asset,
+	destinationAsset: Asset,
+	amount: string,
+	publicKey: string,
+	slippageAmount: string,
+) {
+	const server = new Server(stellarServer);
+	const offers = await findOffers(server, sourceAsset, destinationAsset, amount);
+
+	if (!slippageAmount) {
+		return 'Please, select a slippage tolerance';
+	}
+
+	if (offers.records) {
+		const bestOffer = offers.records[0];
+
+		const paymentOperation = Operation.pathPaymentStrictSend({
+			destination: publicKey,
+			sendAsset: sourceAsset,
+			sendAmount: amount,
+			destAsset: destinationAsset,
+			destMin: getMinDestinationAmount(bestOffer.destination_amount, slippageAmount),
+		});
+
+		const account = await server.loadAccount(publicKey);
+		const transaction = new TransactionBuilder(account, {
+			fee: BASE_FEE,
+			networkPassphrase: Networks.TESTNET,
+		});
+
+		transaction.setTimeout(30);
+		transaction.addOperation(paymentOperation);
+
+		return transaction.build().toXDR();
+	} else {
+		throw new Error();
+	}
+}
+
+function getMinDestinationAmount(offer: string, percentage: string) {
+	const parsedOffer = Number(offer);
+	const parsedPercentage = Number(percentage);
+
+	const result = parsedOffer - parsedOffer * (parsedPercentage / 100);
+
+	return `${result}`;
+}
+
+async function findOffers(
+	server: Server,
+	sourceAsset: Asset,
+	destinationAsset: Asset,
+	amount: string,
+) {
+	const assets = [destinationAsset];
+
+	return await server.strictSendPaths(sourceAsset, amount, assets).call();
 }
