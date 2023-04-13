65b57bf4e23e28ab0cb94cee98c67af1bbb7a32b
anarod13 <anam.rodriguez13@gmail.com>
2023-04-13 14:51:26 -0300

    Add operations to router

--- nodes/Stellar/actions/router.ts
+++ nodes/Stellar/actions/router.ts
@@ -2,27 +2,38 @@ import { IDataObject, IExecuteFunctions, INodeExecutionData } from 'n8n-workflow
 import * as newAccount from './newAccount';
 import * as payments from './payments';
 import * as swapAssets from './swapAssets';
+import * as offers from './offers';
+import * as settings from './settings';
+import * as sponsorship from './sponsorship';
+import * as accountMerge from './accountMerge';
+import * as liquidityPool from './liquidityPool';
 
 export async function router(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
 	const operation = this.getNodeParameter('operation', 0);
 	const resource = this.getNodeParameter('resource', 0) as string;
 	const stellar = { resource, operation };
+	const items = this.getInputData();
 	let outputData = [];
 	let responseData;
+	items.forEach((item) => {
+		if (item.json.operation) {
+			outputData.push(item as IDataObject);
+		}
+	});
 
 	try {
 		switch (stellar.operation) {
 			case 'createAccount':
-				responseData = await newAccount.createAccount.execute.call(this);
+				responseData = await newAccount[stellar.operation].execute.call(this);
 				break;
 			case 'fundAccount':
-				responseData = await newAccount.fundAccount.execute.call(this);
+				responseData = await newAccount[stellar.operation].execute.call(this);
 				break;
 			case 'getPayment':
 				responseData = await payments[stellar.operation].execute.call(this);
 				break;
 			case 'swap':
-				responseData = await swapAssets.swap.execute.call(this);
+				responseData = await swapAssets[stellar.operation].execute.call(this);
 				break;
 			case 'makePayment':
 				responseData = await payments[stellar.operation].execute.call(this);
@@ -33,8 +44,22 @@ export async function router(this: IExecuteFunctions): Promise<INodeExecutionDat
 			case 'pathPaymentStrictReceive':
 				responseData = await payments[stellar.operation].execute.call(this);
 				break;
+			case 'manageSellOffer':
+				responseData = await offers[stellar.operation].execute.call(this);
+				break;
+			case 'accountMerge':
+				responseData = await accountMerge[stellar.operation].execute.call(this);
+				break;
+			case 'manageData':
+				responseData = await settings[stellar.operation].execute.call(this);
+				break;
+			case 'revokeSponsorship':
+				responseData = await sponsorship[stellar.operation].execute.call(this);
+				break;
+			case 'liquidityPoolDeposit':
+				responseData = await liquidityPool[stellar.operation].execute.call(this);
+				break;
 		}
-
 		outputData.push(responseData as IDataObject);
 	} catch (error) {
 		responseData = { error: error.message } as IDataObject;
