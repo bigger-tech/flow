64afaf4ab05f0e3df4017aa6ac8c080e74d93ac9
Ja-boop <56199168+Ja-boop@users.noreply.github.com>
2023-04-19 15:00:20 -0300

    Add try/catch for the execute file

--- nodes/Stellar/actions/transactionBuilder/build/execute.ts
+++ nodes/Stellar/actions/transactionBuilder/build/execute.ts
@@ -3,18 +3,22 @@ import { BASE_FEE, Server, TransactionBuilder } from 'stellar-sdk';
 import { setNetwork } from '../../../transport';
 
 export async function build(this: IExecuteFunctions) {
-	const stellarNetwork = await setNetwork.call(this);
-	const server = new Server(stellarNetwork.url as string);
-	const sourceAccount = this.getNodeParameter('publicKey', 1) as string;
-	const fee = this.getNodeParameter('fee', 1) as string;
-	const timeout = this.getNodeParameter('timeout', 1) as number;
+	try {
+		const stellarNetwork = await setNetwork.call(this);
+		const server = new Server(stellarNetwork.url as string);
+		const sourceAccount = this.getNodeParameter('publicKey', 1) as string;
+		const fee = this.getNodeParameter('fee', 1) as string;
+		const timeout = this.getNodeParameter('timeout', 1) as number;
 
-	const transaction = new TransactionBuilder(await server.loadAccount(sourceAccount), {
-		fee: fee || BASE_FEE,
-		networkPassphrase: stellarNetwork.passphrase,
-	})
-		.setTimeout(timeout | 30)
-		.build();
+		const transaction = new TransactionBuilder(await server.loadAccount(sourceAccount), {
+			fee: fee || BASE_FEE,
+			networkPassphrase: stellarNetwork.passphrase,
+		})
+			.setTimeout(timeout | 30)
+			.build();
 
-	return { transaction: transaction.toXDR() };
+		return { transaction: transaction.toXDR() };
+	} catch (error) {
+		throw new Error(error);
+	}
 }
