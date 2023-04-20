51e5ec37c3c6010a1ba2ae6ae7e44637cbc4f98b
Ja-boop <56199168+Ja-boop@users.noreply.github.com>
2023-04-20 13:43:45 -0300

    Add try/catch when submitting a transaction
    - A failed transaction will return why it failed

--- nodes/Stellar/actions/transaction/sign/execute.ts
+++ nodes/Stellar/actions/transaction/sign/execute.ts
@@ -12,10 +12,10 @@ type SecretKeys = {
 export async function sign(this: IExecuteFunctions) {
 	const stellarNetwork = await setNetwork.call(this);
 	const server = new Server(stellarNetwork.url);
-	const xdr = this.getNodeParameter('xdr', 1) as string;
+	const xdr = this.getNodeParameter('xdr', 0) as string;
 	const secretKeys = this.getNodeParameter('secretKeys', 1) as SecretKeys;
 	const isSubmitToggleOn = this.getNodeParameter('submit', 1) as boolean;
-	const transaction = TransactionBuilder.fromXDR(xdr, stellarNetwork.passphrase);
+	const transaction = TransactionBuilder.fromXDR(xdr as string, stellarNetwork.passphrase);
 	let transactionResult: any;
 
 	for (const account of secretKeys.keys) {
@@ -24,7 +24,11 @@ export async function sign(this: IExecuteFunctions) {
 	}
 
 	if (isSubmitToggleOn) {
-		transactionResult = await server.submitTransaction(transaction);
+		try {
+			transactionResult = await server.submitTransaction(transaction);
+		} catch (error) {
+			throw new Error(error.response.data.extras.result_codes.operations || error);
+		}
 	}
 
 	return { result: transactionResult, transaction: transaction.toXDR() };
