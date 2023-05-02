d2d5956bb4347b0f0752307d2aeb4e17f19216d4
anarod13 <anam.rodriguez13@gmail.com>
2023-05-02 19:17:11 -0300

    Add properties types

--- nodes/Stellar/actions/entities/IStellarNode.ts
+++ nodes/Stellar/actions/entities/IStellarNode.ts
@@ -17,3 +17,31 @@ type StellarMap = {
 };
 
 export type Stellar = AllEntities<StellarMap>;
+
+type StellarAccountMerge = Entity<StellarMap, 'accountMerge'>;
+type StellarSettings = Entity<StellarMap, 'settings'>;
+type StellarClaimableBalance = Entity<StellarMap, 'claimableBalance'>;
+type StellarClawback = Entity<StellarMap, 'clawback'>;
+type StellarFundAccount = Entity<StellarMap, 'fundAccount'>;
+type StellarLiquidityPool = Entity<StellarMap, 'liquidityPool'>;
+type StellarNewAccount = Entity<StellarMap, 'newAccount'>;
+type StellarOffers = Entity<StellarMap, 'offers'>;
+type StellarPayments = Entity<StellarMap, 'payments'>;
+type StellarSponsorship = Entity<StellarMap, 'sponsorship'>;
+type StellarSwapAssets = Entity<StellarMap, 'swapAssets'>;
+type StellarTransaction = Entity<StellarMap, 'transaction'>;
+type StellarTrust = Entity<StellarMap, 'trust'>;
+
+export type AccountMergeProperties = PropertiesOf<StellarAccountMerge>;
+export type SettingsProperties = PropertiesOf<StellarSettings>;
+export type ClaimableBalanceProperties = PropertiesOf<StellarClaimableBalance>;
+export type ClawbackProperties = PropertiesOf<StellarClawback>;
+export type FundAccountProperties = PropertiesOf<StellarFundAccount>;
+export type LiquidityPoolProperties = PropertiesOf<StellarLiquidityPool>;
+export type NewAccountProperties = PropertiesOf<StellarNewAccount>;
+export type OffersProperties = PropertiesOf<StellarOffers>;
+export type PaymentsProperties = PropertiesOf<StellarPayments>;
+export type SponsorshipProperties = PropertiesOf<StellarSponsorship>;
+export type SwapAssetsProperties = PropertiesOf<StellarSwapAssets>;
+export type TransactionProperties = PropertiesOf<StellarTransaction>;
+export type TrustProperties = PropertiesOf<StellarTrust>;
