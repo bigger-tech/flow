774f97cf85a206c817179540f0dac1ba04a53553
anarod13 <anam.rodriguez13@gmail.com>
2023-05-30 13:16:53 -0300

    Refactor setOptions operation

--- nodes/Stellar/actions/settings/setOptions/execute.ts
+++ nodes/Stellar/actions/settings/setOptions/execute.ts
@@ -14,65 +14,63 @@ import ISetOptionsOperationOptions from '../../entities/ISetOptionsOperationOpti
 
 export async function setOptions(this: IExecuteFunctions) {
 	try {
-		let operationOptions: ISetOptionsOperationOptions = {};
 		const inflationDest = this.getNodeParameter('inflationDest', 0) as string;
-		if (inflationDest) operationOptions.inflationDest = inflationDest;
-
-		const flagsToSet = this.getNodeParameter('flagsToSet', 0) as IFlags;
-		if (flagsToSet.values) {
-			let flags = 0;
-			if (flagsToSet.values.authorizationRequired) flags += AuthRequiredFlag;
-			if (flagsToSet.values.authorizationRevocable) flags += AuthRevocableFlag;
-			if (flagsToSet.values.authorizationInmutable) flags += AuthImmutableFlag;
-			if (flagsToSet.values.authorizationClawbackEnabled) flags += AuthClawbackEnabledFlag;
-			operationOptions.setFlags = flags as AuthFlag;
-		}
-
-		const flagsToClear = this.getNodeParameter('flagsToClear', 0) as IFlags;
-		if (flagsToClear.values) {
-			let flags = 0;
-			if (flagsToClear.values.authorizationRequired) flags += AuthRequiredFlag;
-			if (flagsToClear.values.authorizationRevocable) flags += AuthRevocableFlag;
-			if (flagsToClear.values.authorizationClawbackEnabled) flags += AuthClawbackEnabledFlag;
-			operationOptions.clearFlags = flags as AuthFlag;
-		}
-
+		const { values: flagsToSet } = this.getNodeParameter('flagsToSet', 0) as IFlags;
+		const { values: flagsToClear } = this.getNodeParameter('flagsToClear', 0) as IFlags;
 		const masterWeight = this.getNodeParameter('masterWeight', 0) as string;
-		if (masterWeight) operationOptions.masterWeight = Number(masterWeight);
-
 		const lowThreshold = this.getNodeParameter('lowThreshold', 0) as string;
-		if (lowThreshold) operationOptions.lowThreshold = Number(lowThreshold);
-
 		const medThreshold = this.getNodeParameter('medThreshold', 0) as string;
-		if (medThreshold) operationOptions.medThreshold = Number(medThreshold);
-
 		const highThreshold = this.getNodeParameter('highThreshold', 0) as string;
-		if (highThreshold) operationOptions.highThreshold = Number(highThreshold);
-
 		const homeDomain = this.getNodeParameter('homeDomain', 0) as string;
+		const { values: signerValues } = this.getNodeParameter('signer', 0) as ISigner;
+
+		let operationOptions: ISetOptionsOperationOptions = {};
+
+		if (inflationDest) operationOptions.inflationDest = inflationDest;
+		if (flagsToSet) {
+			operationOptions.setFlags = buildFlags(flagsToSet);
+		}
+		if (flagsToClear) {
+			operationOptions.clearFlags = buildFlags(flagsToClear);
+		}
+		if (masterWeight) operationOptions.masterWeight = Number(masterWeight);
+		if (lowThreshold) operationOptions.lowThreshold = Number(lowThreshold);
+		if (medThreshold) operationOptions.medThreshold = Number(medThreshold);
+		if (highThreshold) operationOptions.highThreshold = Number(highThreshold);
 		if (homeDomain) operationOptions.homeDomain = homeDomain;
-
-		const signerInfo = this.getNodeParameter('signer', 0) as ISigner;
-		if (signerInfo.values) {
-			const signerType = signerInfo.values.signerType;
-			const signerKey = signerInfo.values.signerKey;
-			const weight = Number(signerInfo.values.signerWeight);
-			let signer: Signer;
-
-			if (typeof signerKey != 'string' && signerType != 'ed25519PublicKey') {
-				signerType === 'sha256Hash'
-					? (signer = { sha256Hash: signerKey, weight })
-					: (signer = { preAuthTx: signerKey, weight });
-			} else {
-				signer = { ed25519PublicKey: signerKey as string, weight };
-			}
-
-			operationOptions.signer = signer;
+		if (signerValues) {
+			operationOptions.signer = buildSigner(signerValues);
 		}
 
 		const setOptionsOperation = Operation.setOptions(operationOptions).toXDR('base64');
+
 		return { operation: setOptionsOperation };
 	} catch (error) {
 		throw new Error(error);
 	}
+}
+
+function buildFlags(flags: IFlags['values']): AuthFlag {
+	let totalFlags = 0;
+	if (flags.authorizationRequired) totalFlags += AuthRequiredFlag;
+	if (flags.authorizationRevocable) totalFlags += AuthRevocableFlag;
+	if (flags.authorizationInmutable) totalFlags += AuthImmutableFlag;
+	if (flags.authorizationClawbackEnabled) totalFlags += AuthClawbackEnabledFlag;
+	return totalFlags as AuthFlag;
+}
+
+function buildSigner(signerValues: ISigner['values']) {
+	const signerType = signerValues.signerType;
+	const signerKey = signerValues.signerKey;
+	const weight = Number(signerValues.signerWeight);
+
+	let signer: Signer;
+	if (typeof signerKey != 'string' && signerType != 'ed25519PublicKey') {
+		signerType === 'sha256Hash'
+			? (signer = { sha256Hash: signerKey, weight })
+			: (signer = { preAuthTx: signerKey, weight });
+	} else {
+		signer = { ed25519PublicKey: signerKey as string, weight };
+	}
+	return signer;
 }
