17f7285917f7f9647ccb9d3ea8c27a7933aa3ad6
Ja-boop <56199168+Ja-boop@users.noreply.github.com>
2023-06-05 18:15:27 -0300

    Add buildWithdrawTransaction function

--- nodes/Anclap/transport/helpers.ts
+++ nodes/Anclap/transport/helpers.ts
@@ -1,7 +1,54 @@
+import { Asset, BASE_FEE, Operation, Server, TransactionBuilder, Memo } from 'stellar-sdk';
 import { DepositAsset, WithdrawAsset } from './responses/IAnclapInfoResponse';
+import { IAnclapWithdrawResponse } from './responses/responses';
+import CryptoJS from 'crypto-js';
 
 export function verifyAmount(asset: DepositAsset | WithdrawAsset, amount: string) {
 	const minAmount = asset.min_amount;
 	const parsedAmount = Number(amount);
 	return parsedAmount >= minAmount;
+}
+
+export async function buildWithdrawTransaction(
+	publicKey: string,
+	withdrawInfo: IAnclapWithdrawResponse,
+	networkUrl: string,
+	networkPassphrase: string,
+) {
+	const server = new Server(networkUrl);
+	const paymentOp = Operation.payment({
+		destination: withdrawInfo.how,
+		asset: getAsset(),
+		amount: withdrawInfo.amount_in,
+	});
+
+	const transaction = new TransactionBuilder(await server.loadAccount(publicKey), {
+		fee: BASE_FEE,
+		networkPassphrase,
+	})
+		.setTimeout(30)
+		.addOperation(paymentOp)
+		.addMemo(getMemo());
+
+	const transactionXdr = transaction.build().toXDR();
+
+	return transactionXdr;
+
+	function getAsset() {
+		const issuerRegex = new RegExp(/[G][A-Z0-9]{55}/g);
+		const assetCodeRegex = new RegExp(/iso4217:(\w+)/);
+		const issuer = withdrawInfo.amount_in_asset.match(issuerRegex);
+		const assetCode = withdrawInfo.amount_out_asset.match(assetCodeRegex);
+
+		if (issuer && assetCode) {
+			return new Asset(assetCode[1], issuer[0]);
+		} else {
+			throw new Error();
+		}
+	}
+
+	function getMemo() {
+		const memo = CryptoJS.SHA256(withdrawInfo.memo).toString(CryptoJS.enc.Hex);
+		return Memo.hash(memo);
+	}
 }
