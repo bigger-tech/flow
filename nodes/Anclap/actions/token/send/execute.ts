d27e5c695166e0bd3f64853247b6596351ea5d1c
Ja-boop <56199168+Ja-boop@users.noreply.github.com>
2023-06-12 11:10:52 -0300

    Remove throw error from send execute

--- nodes/Anclap/actions/token/send/execute.ts
+++ nodes/Anclap/actions/token/send/execute.ts
@@ -5,12 +5,8 @@ import SEP10 from '../../../transport/SEP10';
 export async function send(this: IExecuteFunctions) {
 	const signedXdr = this.getNodeParameter('signedXdr', 0) as string;
 	const anclapCredentials = new AnclapCredentials(await this.getCredentials('anclapApi'));
+	const sep10 = new SEP10(anclapCredentials);
+	const token = await sep10.sendChallenge(signedXdr);
 
-	try {
-		const sep10 = new SEP10(anclapCredentials);
-		const token = await sep10.sendChallenge(signedXdr);
-		return { token };
-	} catch (e) {
-		throw new Error(e);
-	}
+	return { token };
 }
