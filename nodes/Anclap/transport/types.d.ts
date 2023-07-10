da2bb011957d5326799dd8640eb3604c278a36c1
Gonzalo Altamirano <55992031+GonzaloAlt@users.noreply.github.com>
2023-07-10 19:30:06 -0300

    Add type for OffChain operations

--- nodes/Anclap/transport/types.d.ts
+++ nodes/Anclap/transport/types.d.ts
@@ -1,3 +1,4 @@
 export type Protocol = 'sep6' | 'sep24';
 export type TransactionType = 'default' | 'withdrawal' | 'deposit';
 export type StellarNetwork = 'public' | 'testnet';
+export type OffChainOperationType = 'SEPA'| 'bank_account'| 'cash'; 
