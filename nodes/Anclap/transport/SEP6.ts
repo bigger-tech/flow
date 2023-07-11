49abd08887c62a1e4fb6f003c8897ae0beea812b
Gonzalo Altamirano <55992031+GonzaloAlt@users.noreply.github.com>
2023-07-11 19:51:19 -0300

    Create deposit and withdraw exchange methods and refactor query params

--- nodes/Anclap/transport/SEP6.ts
+++ nodes/Anclap/transport/SEP6.ts
@@ -6,6 +6,10 @@ import WithdrawRequest from './requests/WithdrawRequest/WithdrawRequest';
 import AnclapCredentials from './AnclapCredentials';
 import FeeRequest from './requests/FeeRequest/FeeRequest';
 import queryBuilder from './utils/queryBuilder';
+import IAnclapDepositResponse from './responses/IAnclapDepositResponse';
+import IDepositRequest from './requests/DepositRequest/IDepositRequest';
+import IWithdrawExchangeRequest from './requests/WithdrawRequest/IWithdrawExchangeRequest';
+import IDepositExchangeRequest from './requests/DepositRequest/IDepositExchangeRequest';
 
 export default class SEP6 {
 	private anclapCredentials: AnclapCredentials;
@@ -27,15 +31,18 @@ export default class SEP6 {
 		}
 	}
 
-	async deposit(code: string, amount: string) {
+	async deposit(request: IDepositRequest): Promise<IAnclapDepositResponse> {
 		try {
 			const toml = await this.anclapCredentials.getToml();
-			const info = await axios.get(
-				`${toml.TRANSFER_SERVER}/deposit?asset_code=${code}&account=${this.anclapCredentials.publicKey}&amount=${amount}`,
+
+			request.account = this.anclapCredentials.publicKey;
+			const queryParams = queryBuilder(request);
+			
+			const depositInformation = await axios.get(
+				`${toml.TRANSFER_SERVER}/deposit?${queryParams}`,
 				{ headers: { Authorization: `Bearer ${this.token}` } },
 			);
-
-			return info.data;
+			return depositInformation.data;
 		} catch (e) {
 			throw new AxiosHttpRequestError(e);
 		}
@@ -44,8 +51,12 @@ export default class SEP6 {
 	async withdraw(request: WithdrawRequest): Promise<IAnclapWithdrawResponse> {
 		try {
 			const toml = await this.anclapCredentials.getToml();
+
+			request.account = this.anclapCredentials.publicKey;
+			const queryParams = queryBuilder(request);
+
 			const info = await axios.get(
-				`${toml.TRANSFER_SERVER}/withdraw?asset_code=${request.code}&type=${request.type}&dest=${request.dest}&account=${this.anclapCredentials.publicKey}&amount=${request.amount}`,
+				`${toml.TRANSFER_SERVER}/withdraw?${queryParams}`,
 				{ headers: { Authorization: `Bearer ${this.token}` } },
 			);
 
@@ -58,8 +69,11 @@ export default class SEP6 {
 	async getTransactions(request: TransactionsRequest) {
 		try {
 			const toml = await this.anclapCredentials.getToml();
+
+			const queryParams = queryBuilder(request);
+
 			const info = await axios.get(
-				`${toml.TRANSFER_SERVER}/transactions?asset_code=${request.code}&account=${this.anclapCredentials.publicKey}&kind=${request.kind}`,
+				`${toml.TRANSFER_SERVER}/transactions?account=${this.anclapCredentials.publicKey}&${queryParams}`,
 				{ headers: { Authorization: `Bearer ${this.token}` } },
 			);
 
@@ -85,13 +99,51 @@ export default class SEP6 {
 	async getFee(request: FeeRequest) {
 		try {
 			const toml = await this.anclapCredentials.getToml();
+
 			const queryParams = queryBuilder(request);
+
 			const info = await axios.get(`${toml.TRANSFER_SERVER}/fee?${queryParams}`, {
 				headers: { Authorization: `Bearer ${this.token}` },
 			});
 
 			return info.data;
 		}catch (e) {
+			throw new AxiosHttpRequestError(e);
+		}
+	}
+
+	async getDepositExchange(request: IDepositExchangeRequest): Promise<IAnclapDepositResponse>{
+		try {
+			const toml = await this.anclapCredentials.getToml();
+
+			request.account = this.anclapCredentials.publicKey;
+			const queryParams = queryBuilder(request);
+
+			const depositInformation = await axios.get(
+				`${toml.TRANSFER_SERVER}/deposit-exchange?${queryParams}`,
+				{ headers: { Authorization: `Bearer ${this.token}` } },
+			);
+	
+			return depositInformation.data;
+		} catch (e) {
+			throw new AxiosHttpRequestError(e);
+		}
+	}
+
+	async getWithdrawExchange(request: IWithdrawExchangeRequest): Promise<IAnclapWithdrawResponse>{
+		try {
+			const toml = await this.anclapCredentials.getToml();
+
+			request.account = this.anclapCredentials.publicKey;
+			const queryParams = queryBuilder(request);
+
+			const withdrawInformation = await axios.get(
+				`${toml.TRANSFER_SERVER}/withdraw-exchange?${queryParams}`,
+				{ headers: { Authorization: `Bearer ${this.token}` } },
+			);
+	
+			return withdrawInformation.data;
+		} catch (e) {
 			throw new AxiosHttpRequestError(e);
 		}
 	}
