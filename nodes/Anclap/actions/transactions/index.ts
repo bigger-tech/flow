ce4c197d26f68fa10d4c0767920dd51f60c10059
Gonzalo Altamirano <55992031+GonzaloAlt@users.noreply.github.com>
2023-07-11 20:08:51 -0300

    Update transactions action export file
    - depositInteractive.
    - depositExchange.
    - withdrawExchange.

--- nodes/Anclap/actions/transactions/index.ts
+++ nodes/Anclap/actions/transactions/index.ts
@@ -1,4 +1,7 @@
 import description from './description';
 import { execute as deposit } from './deposit';
 import { execute as withdraw } from './withdraw';
-export { deposit, withdraw, description };
+import { execute as depositInteractive } from './deposit-interactive';
+import { execute as depositExchange } from './deposit-exchange';
+import { execute as withdrawExchange } from './withdraw-exchange';
+export { deposit, withdraw, depositInteractive, depositExchange, withdrawExchange, description };
