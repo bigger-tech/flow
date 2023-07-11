f3e356912d18fc3a97a9683cc1c7878f0c464404
Gonzalo Altamirano <55992031+GonzaloAlt@users.noreply.github.com>
2023-07-11 20:07:28 -0300

    Update transactions description with the following
    - Deposit interactive for deposit flow outside n8n UI.
    - Deposit exchange for non-equivalent assets deposit.
    - Withdraw exchange for non-equivalent assets withdrawals.

--- nodes/Anclap/actions/transactions/description.ts
+++ nodes/Anclap/actions/transactions/description.ts
@@ -1,6 +1,9 @@
 import { INodeProperties } from 'n8n-workflow';
 import { description as depositDescription } from './deposit';
 import { description as withdrawDescription } from './withdraw';
+import { description as depositInteractiveDescription } from './deposit-interactive';
+import { description as depositExchangeDescription } from './deposit-exchange';
+import { description as withdrawExchangeDescription } from './withdraw-exchange';
 
 const description: INodeProperties[] = [
 	{
@@ -25,10 +28,28 @@ const description: INodeProperties[] = [
 				value: 'withdraw',
 				action: 'Withdraw',
 			},
+			{
+				name: 'Deposit Interactive',
+				value: 'depositInteractive',
+				action: 'Deposit Interactive',
+			},
+			{
+				name: 'Deposit Exchange',
+				value: 'depositExchange',
+				action: 'Deposit Exchange',
+			},
+			{
+				name: 'Withdraw Exchange',
+				value: 'withdrawExchange',
+				action: 'Withdraw Exchange',
+			},
 		],
 	},
 	...depositDescription,
 	...withdrawDescription,
+	...depositInteractiveDescription,
+	...depositExchangeDescription,
+	...withdrawExchangeDescription
 ];
 
 export default description;
