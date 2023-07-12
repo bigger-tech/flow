404fca859168353bd43400cfd3e6e1a467c47315
Gonzalo Altamirano <55992031+GonzaloAlt@users.noreply.github.com>
2023-07-12 20:20:43 -0300

    Update transaction execute with optional values

--- nodes/Anclap/actions/info/transaction/execute.ts
+++ nodes/Anclap/actions/info/transaction/execute.ts
@@ -3,26 +3,55 @@ import SEP6 from '../../../transport/SEP6';
 import SEP24 from '../../../transport/SEP24';
 import AnclapCredentials from '../../../transport/AnclapCredentials';
 import { Protocol } from '../../../transport/types';
+import TransactionRequest from '../../../transport/requests/TransactionRequest/TransactionRequest';
+import ITransactionRequest from '../../../transport/requests/TransactionRequest/ITransactionRequest';
 
 export async function transaction(this: IExecuteFunctions) {
 	const anclapCredentials = new AnclapCredentials(await this.getCredentials('anclapApi'));
 	const protocol = this.getNodeParameter('protocol', 0) as Protocol;
 	const token = this.getNodeParameter('token', 0) as string;
+
 	const id = this.getNodeParameter('id', 0) as string;
+
+	const showOptionalValues = this.getNodeParameter('showOptionalValues', 0) as boolean;
+
+	let transactionRequest: ITransactionRequest
+
+	if(showOptionalValues){
+		const stellarTransactionId = this.getNodeParameter('stellarTransactionId', 0) as string;
+		const externalTransactionId = this.getNodeParameter('externalTransactionId', 0) as string;
+		const lang = this.getNodeParameter('lang', 0) as string;
+
+		if(!id && !stellarTransactionId && !externalTransactionId){
+			throw new Error('On of id or stellar Transaction Id or external Transaction Id is required');
+		}
+
+		transactionRequest = new TransactionRequest({
+			id,
+			stellarTransactionId,
+			externalTransactionId,
+			lang,
+		});
+	}else{
+		if(!id){
+			throw new Error('On of id or stellar Transaction Id or external Transaction Id is required');
+		}
+		transactionRequest = new TransactionRequest({id});
+	}
+
+	async function getSep24Transaction() {
+		const sep24 = new SEP24(anclapCredentials, token);
+		return await sep24.getTransactionById(transactionRequest);
+	}
+
+	async function getSep6Transaction() {
+		const sep6 = new SEP6(anclapCredentials, token);
+		return await sep6.getTransactionById(transactionRequest);
+	}
 
 	if (protocol === 'sep24') {
 		return await getSep24Transaction();
 	} else {
 		return await getSep6Transaction();
-	}
-
-	async function getSep24Transaction() {
-		const sep24 = new SEP24(anclapCredentials, token);
-		return await sep24.getTransactionById(id);
-	}
-
-	async function getSep6Transaction() {
-		const sep6 = new SEP6(anclapCredentials, token);
-		return await sep6.getTransactionById(id);
 	}
 }
