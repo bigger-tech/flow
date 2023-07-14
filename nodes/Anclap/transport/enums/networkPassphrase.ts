31aeb6ac09b0234a5204f37f15b1351eb7e41908
Gonzalo Altamirano <55992031+GonzaloAlt@users.noreply.github.com>
2023-07-14 16:56:39 -0300

    Create networkPassphrase enum

---
+++ nodes/Anclap/transport/enums/networkPassphrase.ts
@@ -0,0 +1,5 @@
+export enum networkPassphrase {
+	testnet = 'Test SDF Network ; September 2015',
+	public = 'Public Global Stellar Network ; September 2015',
+	futurenet = 'Test SDF Future Network ; October 2022',
+}
