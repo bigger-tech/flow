ca80b84ac031a9d4af7f1388a0922a2ccbc0fb75
Gonzalo Altamirano <55992031+GonzaloAlt@users.noreply.github.com>
2023-07-14 16:57:41 -0300

    Add validation and sign helpers for anchor challenge

--- nodes/Anclap/transport/helpers.ts
+++ nodes/Anclap/transport/helpers.ts
@@ -1,7 +1,18 @@
-import { Asset, Operation, Server, TransactionBuilder, Memo } from 'stellar-sdk';
+import {
+	Asset,
+	Operation,
+	Server,
+	TransactionBuilder,
+	Memo,
+	Transaction,
+	Keypair,
+} from 'stellar-sdk';
 import { DepositAsset, WithdrawAsset } from './responses/IAnclapTransferServerInfoResponse';
-import { IAnclapWithdrawResponse } from './responses/responses';
+import { IAnclapWithdrawResponse, IAnclapTomlResponse } from './responses/responses';
 import GetAssetError from './errors/GetAssetError';
+import GetChallengeValidationError from './errors/GetChallengeValidationError';
+import AnclapCredentials from './AnclapCredentials';
+import { networkPassphrase } from './enums/networkPassphrase';
 
 export function verifyAmount(asset: DepositAsset | WithdrawAsset, amount: string): boolean {
 	const minAmount = asset.min_amount;
@@ -51,4 +62,85 @@ export async function buildWithdrawTransaction(
 
 		return Memo.hash(hex);
 	}
+}
+
+export async function validateXdrProvenance(
+	anchor: IAnclapTomlResponse,
+	challengeXdr: string,
+	clientPublicKey: string,
+): Promise<object> {
+	const transaction = new Transaction(challengeXdr, 'base64');
+	const anchorHomeDomain = extractHomeDomain(anchor.WEB_AUTH_ENDPOINT);
+
+	if (transaction.sequence !== '0')
+		throw new GetChallengeValidationError('Invalid sequence number');
+
+	if (transaction.source !== anchor.SIGNING_KEY)
+		throw new GetChallengeValidationError('Transaction must be signed by the Server');
+
+	const operations = transaction.operations;
+	const firstManageDataOperation = operations[0] as Operation.ManageData;
+
+	if (firstManageDataOperation.source !== clientPublicKey)
+		throw new GetChallengeValidationError(
+			'First manage data operation must have Source account set to the Client Account',
+		);
+
+	if (firstManageDataOperation.name !== `${anchorHomeDomain} auth`)
+		throw new GetChallengeValidationError(
+			`First manage data operation must have Key set to ${anchorHomeDomain} auth`,
+		);
+
+	const webAuthDomain = [] as Operation.ManageData[];
+	for (let operation of operations) {
+		operation = operation as Operation.ManageData;
+		if (operation.name === 'web_auth_domain') webAuthDomain.push(operation);
+	}
+
+	if (webAuthDomain.length <= 0 || webAuthDomain[0]?.source !== anchor.SIGNING_KEY)
+		throw new GetChallengeValidationError(
+			`Source account in manageData must be set to the Server account: ${anchor.SIGNING_KEY}`,
+		);
+
+	if (
+		webAuthDomain.length >= 1 &&
+		!webAuthDomain.every(
+			(operation) => Buffer.from(operation.value ?? '').toString('utf8') === anchorHomeDomain,
+		)
+	)
+		throw new GetChallengeValidationError(
+			'Value of operation with name "web_auth_domain" must be the Server domain',
+		);
+
+	if (operations.length > 2) {
+		const additionalOperations = operations.slice(2);
+		const isValidManageData = additionalOperations.every(
+			(operation) => operation.source === anchor.SIGNING_KEY,
+		);
+		if (!isValidManageData)
+			throw new GetChallengeValidationError(
+				'There is an invalid additional manage data in the operation',
+			);
+	}
+
+	return { xdr: challengeXdr };
+}
+
+export function signXdr(challengeXdr: string, anclapCredentials: AnclapCredentials): object {
+	const transaction = TransactionBuilder.fromXDR(
+		challengeXdr,
+		networkPassphrase[anclapCredentials.stellarNetwork],
+	);
+	transaction.sign(Keypair.fromSecret(anclapCredentials.secretKey));
+
+	return { signedXdr: transaction.toXDR() };
+}
+
+function extractHomeDomain(url: string): string {
+	const domainRegex = /^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/g;
+	const matches = domainRegex.exec(url);
+	if (matches && matches.length >= 2) {
+		return matches[1];
+	}
+	return '';
 }
