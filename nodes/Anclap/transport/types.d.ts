cbb3566374bd46806d7ccfbb4b854d1993ab5c0b
Gonzalo Altamirano <55992031+GonzaloAlt@users.noreply.github.com>
2023-07-17 18:21:32 -0300

    Change Protocol type to enum to avoid magic strings

--- nodes/Anclap/transport/types.d.ts
+++ nodes/Anclap/transport/types.d.ts
@@ -1,4 +1,3 @@
-export type Protocol = 'sep6' | 'sep24';
 export type TransactionType = 'withdrawal' | 'deposit' | 'deposit-exchange' | 'withdrawal-exchange';
 export type StellarNetwork = 'public' | 'testnet';
 export type OffChainOperationType = 'SEPA' | 'bank_account' | 'cash';
