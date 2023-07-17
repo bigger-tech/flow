4d4ab4c91dbf04e48fde2ba4f1bb0194881b9666
Gonzalo Altamirano <55992031+GonzaloAlt@users.noreply.github.com>
2023-07-17 13:29:30 -0300

    Update deposit interactive with optional values

--- nodes/Anclap/actions/transactions/deposit-interactive/description.ts
+++ nodes/Anclap/actions/transactions/deposit-interactive/description.ts
@@ -29,4 +29,191 @@ export const depositInteractiveDescription: INodeProperties[] = [
 		description:
 			'The code of the on-chain asset the user wants to get from the Anchor after doing an off-chain deposit.',
 	},
+	{
+		displayName: 'Amount',
+		name: 'amount',
+		type: 'number',
+		required: true,
+		displayOptions: {
+			show: {
+				resource: ['transactions'],
+				operation: ['depositInteractive'],
+			},
+		},
+		default: '',
+		description:
+			'The amount of the asset the user would like to deposit with the anchor. This field may be necessary for the anchor to determine what KYC information is necessary to collect.',
+	},
+	{
+		displayName: 'Type',
+		name: 'type',
+		type: 'string',
+		required: true,
+		displayOptions: {
+			show: {
+				resource: ['transactions'],
+				operation: ['depositInteractive'],
+			},
+		},
+		default: 'bank_account',
+		description:
+			'Type of deposit. If the anchor supports multiple deposit methods (e.g. SEPA or SWIFT), the wallet should specify type. This field may be necessary for the anchor to determine which KYC fields to collect.',
+	},
+	{
+		displayName: 'Show Optional Values',
+		name: 'showOptionalValues',
+		type: 'boolean',
+		required: true,
+		displayOptions: {
+			show: {
+				resource: ['transactions'],
+				operation: ['depositInteractive'],
+			},
+		},
+		default: false,
+	},
+	{
+		displayName: 'Memo Type',
+		name: 'memoType',
+		type: 'string',
+		required: false,
+		displayOptions: {
+			show: {
+				showOptionalValues: [true],
+				resource: ['transactions'],
+				operation: ['depositInteractive'],
+			},
+		},
+		default: '',
+		description:
+			'Type of memo that the anchor should attach to the Stellar payment transaction, one of text, id or hash.',
+	},
+	{
+		displayName: 'Memo',
+		name: 'memo',
+		type: 'string',
+		required: false,
+		displayOptions: {
+			show: {
+				showOptionalValues: [true],
+				resource: ['transactions'],
+				operation: ['depositInteractive'],
+			},
+		},
+		default: '',
+		description:
+			'Value of memo to attach to transaction, for hash this should be base64-encoded. Because a memo can be specified in the SEP-10 JWT for Shared Accounts, this field as well as memo_type can be different than the values included in the SEP-10 JWT. For example, a client application could use the value passed for this parameter as a reference number used to match payments made to account.',
+	},
+	{
+		displayName: 'Email Address',
+		name: 'emailAddress',
+		type: 'string',
+		required: false,
+		displayOptions: {
+			show: {
+				showOptionalValues: [true],
+				resource: ['transactions'],
+				operation: ['depositInteractive'],
+			},
+		},
+		default: '',
+		description:
+			'Email address of depositor. If desired, an anchor can use this to send email updates to the user about the deposit.',
+	},
+	{
+		displayName: 'Wallet Name',
+		name: 'walletName',
+		type: 'string',
+		required: false,
+		displayOptions: {
+			show: {
+				showOptionalValues: [true],
+				resource: ['transactions'],
+				operation: ['depositInteractive'],
+			},
+		},
+		default: '',
+		description:
+			'In communications / pages about the deposit, anchor should display the wallet name to the user to explain where funds are going.',
+	},
+	{
+		displayName: 'Wallet Url',
+		name: 'walletUrl',
+		type: 'string',
+		required: false,
+		displayOptions: {
+			show: {
+				showOptionalValues: [true],
+				resource: ['transactions'],
+				operation: ['depositInteractive'],
+			},
+		},
+		default: '',
+		description:
+			'Anchor should link to this when notifying the user that the transaction has completed.',
+	},
+	{
+		displayName: 'Language',
+		name: 'lang',
+		type: 'string',
+		required: false,
+		displayOptions: {
+			show: {
+				showOptionalValues: [true],
+				resource: ['transactions'],
+				operation: ['depositInteractive'],
+			},
+		},
+		default: '',
+		description:
+			'Defaults to EN if not specified or if the specified language is not supported. Language code specified using RFC 4646. error fields and other human readable messages in the response should be in this language.',
+	},
+	{
+		displayName: 'Callback Url',
+		name: 'onChangeCallback',
+		type: 'string',
+		required: false,
+		displayOptions: {
+			show: {
+				showOptionalValues: [true],
+				resource: ['transactions'],
+				operation: ['depositInteractive'],
+			},
+		},
+		default: '',
+		description:
+			'A URL that the anchor should POST a JSON message to when the status property of the transaction created as a result of this request changes. The JSON message should be identical to the response format for the /transaction endpoint. The callback needs to be signed by the anchor and the signature needs to be verified by the wallet according to the callback signature specification.',
+	},
+	{
+		displayName: 'Country Code',
+		name: 'countryCode',
+		type: 'string',
+		required: false,
+		displayOptions: {
+			show: {
+				showOptionalValues: [true],
+				resource: ['transactions'],
+				operation: ['depositInteractive'],
+			},
+		},
+		default: '',
+		description:
+			"The ISO 3166-1 alpha-3 code of the user's current address. This field may be necessary for the anchor to determine what KYC information is necessary to collect.",
+	},
+	{
+		displayName: 'Claimable balance supported',
+		name: 'claimableBalanceSupported',
+		type: 'boolean',
+		required: false,
+		displayOptions: {
+			show: {
+				showOptionalValues: [true],
+				resource: ['transactions'],
+				operation: ['depositInteractive'],
+			},
+		},
+		default: 'false',
+		description:
+			'true if the client supports receiving deposit transactions as a claimable balance, false otherwise.',
+	},
 ];
--- nodes/Anclap/actions/transactions/deposit-interactive/execute.ts
+++ nodes/Anclap/actions/transactions/deposit-interactive/execute.ts
@@ -1,16 +1,55 @@
 import { IExecuteFunctions } from 'n8n-workflow';
 import AnclapCredentials from '../../../transport/AnclapCredentials';
-import { AnclapAssetCode } from '../../../transport/responses/IAnclapTransferServerInfoResponse';
 import SEP24 from '../../../transport/SEP24';
+import DepositRequest from '../../../transport/requests/DepositRequest/DepositRequest';
+import IDepositRequest from '../../../transport/requests/DepositRequest/IDepositRequest';
 
 export async function depositInteractive(this: IExecuteFunctions) {
 	const anclapCredentials = new AnclapCredentials(await this.getCredentials('anclapApi'));
 	const token = this.getNodeParameter('token', 0) as string;
-	const assetCode = this.getNodeParameter('assetCode', 0) as AnclapAssetCode;
+
+	const amount = this.getNodeParameter('amount', 0) as string;
+	const assetCode = this.getNodeParameter('assetCode', 0) as string;
+	const type = this.getNodeParameter('type', 0) as string;
+
+	const showOptionalValues = this.getNodeParameter('showOptionalValues', 0) as boolean;
 
 	const getSep24DepositUrl = async () => {
 		const sep24 = new SEP24(anclapCredentials, token);
-		return await sep24.getDepositInteractiveUrl(assetCode);
+
+		let depositRequest: IDepositRequest;
+
+		if (showOptionalValues) {
+			const memoType = this?.getNodeParameter('memoType', 0) as string;
+			const memo = this?.getNodeParameter('memo', 0) as string;
+			const walletName = this?.getNodeParameter('walletName', 0) as string;
+			const walletUrl = this?.getNodeParameter('walletUrl', 0) as string;
+			const lang = this?.getNodeParameter('lang', 0) as string;
+			const onChangeCallback = this?.getNodeParameter('onChangeCallback', 0) as string;
+			const countryCode = this?.getNodeParameter('countryCode', 0) as string;
+			const claimableBalanceSupported = this?.getNodeParameter(
+				'claimableBalanceSupported',
+				0,
+			) as string;
+
+			depositRequest = new DepositRequest({
+				assetCode,
+				amount,
+				memoType,
+				memo,
+				type,
+				walletName,
+				walletUrl,
+				lang,
+				onChangeCallback,
+				countryCode,
+				claimableBalanceSupported,
+			});
+		} else {
+			depositRequest = new DepositRequest({ assetCode, amount, type });
+		}
+
+		return await sep24.getDepositInteractiveUrl(depositRequest);
 	};
 
 	return await getSep24DepositUrl();
