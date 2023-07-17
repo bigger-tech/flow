ac9a92fab426bddf94f923a60a74adc810b47522
Gonzalo Altamirano <55992031+GonzaloAlt@users.noreply.github.com>
2023-07-17 18:27:59 -0300

    Update SEP6 and SEP24 imports

--- nodes/Anclap/actions/info/transactions/index.ts
+++ nodes/Anclap/actions/info/transactions/index.ts
@@ -1,4 +1,4 @@
 import { transactionsDescription as description } from './description';
-import { transactions as execute } from './execute';
+import { getTransactions as execute } from './execute';
 
 export { description, execute };
