2736773b43c93291dd2c59581f03a02c7e77b16e
Gonzalo Altamirano <55992031+GonzaloAlt@users.noreply.github.com>
2023-07-19 17:05:43 -0300

    Update networkpassphrase enum to PascalCase

--- nodes/Anclap/transport/enums/networkPassphrase.ts
+++ nodes/Anclap/transport/enums/networkPassphrase.ts
@@ -1,4 +1,4 @@
-export enum networkPassphrase {
+export enum NetworkPassphrase {
 	testnet = 'Test SDF Network ; September 2015',
 	public = 'Public Global Stellar Network ; September 2015',
 	futurenet = 'Test SDF Future Network ; October 2022',
