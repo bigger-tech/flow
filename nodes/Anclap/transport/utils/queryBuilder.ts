4f98693d46ed915da1a9fb9e3a33b011347d7397
Gonzalo Altamirano <55992031+GonzaloAlt@users.noreply.github.com>
2023-07-19 10:42:52 -0300

    Move object parsers into PayloadBuilder

--- nodes/Anclap/transport/utils/queryBuilder.ts
+++ nodes/Anclap/transport/utils/queryBuilder.ts
@@ -1,21 +1,10 @@
+import PayloadBuilder from './PayloadBuilder';
 import convertToSnakeCase from './convertToSnakeCase';
 
 export default function queryBuilder(input: Record<string, any>): string {
-	const filteredInput = filterUndefinedValues(input);
+	const filteredInput = new PayloadBuilder(input).filterUndefinedValues().build();
 
 	return Object.entries(filteredInput)
 		.map(([key, value]) => `${convertToSnakeCase(key)}=${encodeURIComponent(value)}`)
 		.join('&');
-}
-
-function filterUndefinedValues(input: Record<string, any>): Record<string, any> {
-	const filtered: Record<string, any> = {};
-
-	for (const [key, value] of Object.entries(input)) {
-		if (value !== undefined && value.length !== 0) {
-			filtered[key] = value;
-		}
-	}
-
-	return filtered;
 }
