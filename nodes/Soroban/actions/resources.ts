8661be1d08fa624015910c75b5ba5b1497b9a4b4
Andres Nazzari <lordcliff@gmail.com>
2023-10-24 05:48:16 +0900

    Create soroban resource

---
+++ nodes/Soroban/actions/resources.ts
@@ -0,0 +1,14 @@
+import { SorobanResources } from './entities/ISorobanNode';
+
+interface IOperations {
+	operations: { [key: string]: { execute: () => Promise<{}> | {} } };
+}
+
+const resources: { [key in keyof SorobanResources]: IOperations } = {
+	newAccount: {
+		operations: {
+		},
+	},
+};
+
+export default resources;
