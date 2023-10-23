9365327b1bc258862c6309a535c1b2b301624029
Andres Nazzari <lordcliff@gmail.com>
2023-10-24 05:47:52 +0900

    Create soroban router

---
+++ nodes/Soroban/actions/router.ts
@@ -0,0 +1,27 @@
+import { IDataObject, IExecuteFunctions, INodeExecutionData } from 'n8n-workflow';
+import { Soroban } from './entities/ISorobanNode';
+import resources from './resources';
+
+export async function router(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+	const operation = this.getNodeParameter('operation', 0);
+	const resource = this.getNodeParameter('resource', 0) as string;
+	const soroban = { resource, operation } as Soroban;
+	const items = this.getInputData();
+	let nodeOutput: IDataObject[] = [];
+	let response;
+
+	for (const item of items) {
+		if ( item.json.operation ) {
+			nodeOutput.push(item);
+		}
+	}
+
+	try {
+		response = await resources[soroban.resource].operations[soroban.operation].execute.call(this);
+		nodeOutput.push(response as IDataObject);
+	} catch (error) {
+		throw new Error(error);
+	}
+
+	return [this.helpers.returnJsonArray(nodeOutput)];
+}
