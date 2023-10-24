6f8a511fb3893e25ebe815c9a423702a8c747142
Andres Nazzari <lordcliff@gmail.com>
2023-10-25 05:42:44 +0900

    Create buildAsset and convertAmountToBigNumber to be reusable

--- nodes/Stellar/transport/index.ts
+++ nodes/Stellar/transport/index.ts
@@ -1,9 +1,5 @@
 import type { IExecuteFunctions, ITriggerFunctions } from 'n8n-workflow';
-import BigNumber from 'bignumber.js';
-import SetNetworkError from './errors/SetNetworkError';
-import NoAssetSelectedError from './errors/NoAssetSelectedError';
-import IAsset from '../actions/entities/IAsset';
-import { Asset } from 'stellar-sdk';
+import SetNetworkError from '../../../common/errors/stellarBlockchain/SetNetworkError';
 
 const STELLAR_TESTNET_NETWORK = 'https://horizon-testnet.stellar.org/';
 const STELLAR_TESTNET_PASSPHRASE = 'Test SDF Network ; September 2015';
@@ -22,7 +18,6 @@ export async function setNetwork(
 			break;
 		case 'pubnet':
 			stellarNetwork = new StellarNetwork(STELLAR_PUBNET_NETWORK, STELLAR_PUBNET_PASSPHRASE);
-			break;
 			break;
 		case 'custom':
 			stellarNetwork = new StellarNetwork(networkUrl as string, networkPassphrase as string);
@@ -43,19 +38,4 @@ class StellarNetwork {
 		this.url = networkUrl;
 		this.passphrase = networkPassphrase;
 	}
-}
-
-export function convertAmountToBigNumber(amount: number): string {
-	return new BigNumber(amount).toFixed(7).toString();
-}
-
-export function buildAsset(assetValues: IAsset['values']): Asset {
-	let asset: Asset;
-	if (!assetValues) throw new NoAssetSelectedError('An asset must be selected');
-	if (assetValues.isNative) {
-		asset = Asset.native();
-	} else {
-		asset = new Asset(assetValues.code, assetValues.issuer);
-	}
-	return asset;
 }
