b3a134e985ac1bca82ca30e17d148e5783e605e5
Gonzalo Altamirano <55992031+GonzaloAlt@users.noreply.github.com>
2023-10-24 10:45:06 -0300

    Remove futurenet from Stellar index.ts

--- nodes/Stellar/transport/index.ts
+++ nodes/Stellar/transport/index.ts
@@ -9,29 +9,25 @@ const STELLAR_TESTNET_NETWORK = 'https://horizon-testnet.stellar.org/';
 const STELLAR_TESTNET_PASSPHRASE = 'Test SDF Network ; September 2015';
 const STELLAR_PUBNET_NETWORK = 'https://horizon.stellar.org/';
 const STELLAR_PUBNET_PASSPHRASE = 'Public Global Stellar Network ; September 2015';
-const STELLAR_FUTURENET_NETWORK = 'https://horizon-futurenet.stellar.org';
-const STELLAR_FUTURENET_PASSPHRASE = 'Test SDF Future Network ; October 2022';
 
 export async function setNetwork(
 	this: IExecuteFunctions | ITriggerFunctions,
 ): Promise<StellarNetwork> {
-	const credentials = await this.getCredentials('stellarNetworkApi');
+	const {network, networkUrl, networkPassphrase} = await this.getCredentials('stellarNetworkApi');
 	let stellarNetwork;
 
-	switch (credentials.network) {
+	switch (network) {
 		case 'testnet':
 			stellarNetwork = new StellarNetwork(STELLAR_TESTNET_NETWORK, STELLAR_TESTNET_PASSPHRASE);
 			break;
 		case 'pubnet':
 			stellarNetwork = new StellarNetwork(STELLAR_PUBNET_NETWORK, STELLAR_PUBNET_PASSPHRASE);
 			break;
-		case 'futurenet':
-			stellarNetwork = new StellarNetwork(STELLAR_FUTURENET_NETWORK, STELLAR_FUTURENET_PASSPHRASE);
 			break;
 		case 'custom':
 			stellarNetwork = new StellarNetwork(
-				credentials.networkUrl as string,
-				credentials.networkPassphrase as string,
+				networkUrl as string,
+				networkPassphrase as string,
 			);
 	}
 
