55344b33cf3bb55594d6da958b5e275828b8d100
Andres Nazzari <lordcliff@gmail.com>
2023-10-26 05:37:51 +0900

    Create fundAccount action node

---
+++ nodes/Soroban/actions/fundAccount/description.ts
@@ -0,0 +1,26 @@
+import { FundAccountProperties } from '../entities/SorobanNode';
+
+const description: FundAccountProperties = [
+	{
+		displayName: 'Operation',
+		name: 'operation',
+		type: 'options',
+		default: 'fundAccount',
+		noDataExpression: true,
+		displayOptions: {
+			show: {
+				resource: ['fundAccount'],
+			},
+		},
+		options: [
+			{
+				name: 'Fund Account with Friendbot',
+				value: 'fundAccount',
+				description: 'Get testnet lumens',
+				action: 'Fund account in testnet',
+			},
+		],
+	},
+];
+
+export default description;
---
+++ nodes/Soroban/actions/fundAccount/fundAccount/execute.ts
@@ -0,0 +1,25 @@
+import StellarSdk from 'stellar-sdk';
+import type { IExecuteFunctions, IHttpRequestOptions } from 'n8n-workflow';
+import InvalidPublicKeyError from '../../../../../common/errors/stellar/InvalidPublicKeyError';
+
+const FRIENDBOT_URL = 'https://friendbot-futurenet.stellar.org/?addr=';
+
+export async function fundAccount(this: IExecuteFunctions) {
+	try {
+		const items = this.getInputData();
+		const publicKey = items[0].json.publicKey as string;
+
+		if (StellarSdk.StrKey.isValidEd25519PublicKey(publicKey)) {
+			const fundAccountRequest: IHttpRequestOptions = {
+				url: `${FRIENDBOT_URL}${publicKey}`,
+				method: 'GET',
+			};
+			await this.helpers.httpRequest(fundAccountRequest);
+		} else {
+			throw new InvalidPublicKeyError('Invalid public key');
+		}
+		return { accountFunded: publicKey };
+	} catch (error) {
+		throw new Error(error);
+	}
+}
