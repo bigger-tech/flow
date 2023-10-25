e2fafd7c804fbca3e9f3fa7d4e08a841b49e1654
Andres Nazzari <lordcliff@gmail.com>
2023-10-26 03:09:12 +0900

    Update sorobanEntities with payments

--- nodes/Soroban/actions/entities/ISorobanNode.ts
+++ nodes/Soroban/actions/entities/ISorobanNode.ts
@@ -2,10 +2,13 @@ import type { AllEntities, Entity, PropertiesOf } from 'n8n-workflow';
 
 export type SorobanResources = {
 	newAccount: 'createAccount';
+	payments: 'getPayment' | 'makePayment' | 'pathPaymentStrictSend' | 'pathPaymentStrictReceive';
 };
 
 export type Soroban = AllEntities<SorobanResources>;
 
 type SorobanNewAccount = Entity<SorobanResources, 'newAccount'>;
+type SorobanPayments = Entity<SorobanResources, 'payments'>;
 
 export type NewAccountProperties = PropertiesOf<SorobanNewAccount>;
+export type PaymentsProperties = PropertiesOf<SorobanPayments>;
