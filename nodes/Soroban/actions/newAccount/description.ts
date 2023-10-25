17bf49ab07e0c8dd7dc674ae49112e7bd519bda4
Andres Nazzari <lordcliff@gmail.com>
2023-10-25 23:55:15 +0900

    Create soroban newAccount action

---
+++ nodes/Soroban/actions/newAccount/description.ts
@@ -0,0 +1,26 @@
+import { NewAccountProperties } from '../entities/ISorobanNode';
+
+const description: NewAccountProperties = [
+	{
+		displayName: 'Operation',
+		name: 'operation',
+		type: 'options',
+		default: 'createAccount',
+		noDataExpression: true,
+		displayOptions: {
+			show: {
+				resource: ['newAccount'],
+			},
+		},
+		options: [
+			{
+				name: 'Create Account',
+				value: 'createAccount',
+				description: 'Create a new Soroban account',
+				action: 'Create a new soroban account',
+			},
+		],
+	},
+];
+
+export default description;
