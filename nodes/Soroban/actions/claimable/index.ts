5f82c8c1ee09bc70c8c905846c9ebe5fb2af5b63
Andres Nazzari <lordcliff@gmail.com>
2023-10-27 02:12:31 +0900

    Create claimable actions descripionn

---
+++ nodes/Soroban/actions/claimable/index.ts
@@ -0,0 +1,5 @@
+import * as claimClaimableBalance from './claimClaimableBalance';
+import * as createClaimableBalance from './createClaimableBalance';
+import description from './description';
+
+export { claimClaimableBalance, createClaimableBalance, description };
--- nodes/Soroban/actions/entities/SorobanNode.ts
+++ nodes/Soroban/actions/entities/SorobanNode.ts
@@ -1,6 +1,7 @@
 import type { AllEntities, Entity, PropertiesOf } from 'n8n-workflow';
 
 export type SorobanResources = {
+	claimableBalance: 'createClaimableBalance' | 'claimClaimableBalance';
 	newAccount: 'createAccount';
 	transaction: 'build' | 'sign';
 	payments: 'getPayment' | 'makePayment' | 'pathPaymentStrictSend' | 'pathPaymentStrictReceive';
@@ -9,11 +10,13 @@ export type SorobanResources = {
 
 export type Soroban = AllEntities<SorobanResources>;
 
+type SorobanClaimableBalance = Entity<SorobanResources, 'claimableBalance'>;
 type SorobanNewAccount = Entity<SorobanResources, 'newAccount'>;
 type SorobanTransaction = Entity<SorobanResources, 'transaction'>;
 type SorobanPayments = Entity<SorobanResources, 'payments'>;
 type SorobanFundAccount = Entity<SorobanResources, 'fundAccount'>;
 
+export type ClaimableBalanceProperties = PropertiesOf<SorobanClaimableBalance>;
 export type NewAccountProperties = PropertiesOf<SorobanNewAccount>;
 export type TransactionProperties = PropertiesOf<SorobanTransaction>;
 export type PaymentsProperties = PropertiesOf<SorobanPayments>;
