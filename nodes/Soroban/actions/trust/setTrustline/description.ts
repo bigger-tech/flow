6b7ee45b5c2308cb4c4e921c3f07f1effb30ae43
Gonzalo Altamirano <55992031+GonzaloAlt@users.noreply.github.com>
2023-10-26 12:13:29 -0300

    Create setTrustline soroban node

---
+++ nodes/Soroban/actions/trust/setTrustline/description.ts
@@ -0,0 +1,94 @@
+import { TrustProperties } from '../../entities/SorobanNode';
+
+export const setTrustlineDescription: TrustProperties = [
+	{
+		displayName: 'Trustor',
+		name: 'trustor',
+		type: 'string',
+		required: true,
+		displayOptions: {
+			show: {
+				resource: ['trust'],
+				operation: ['setTrustline'],
+			},
+		},
+		default: '',
+		placeholder: 'GCEVJ...',
+		description: 'Account public key',
+	},
+	{
+		displayName: 'Asset',
+		name: 'trustAsset',
+		type: 'fixedCollection',
+		default: {},
+		required: true,
+		options: [
+			{
+				name: 'values',
+				displayName: 'Asset',
+				values: [
+					{
+						displayName: 'Code',
+						name: 'code',
+						type: 'string',
+						default: '',
+					},
+					{
+						displayName: 'Issuer',
+						name: 'issuer',
+						type: 'string',
+						default: '',
+						placeholder: 'GCEVJ...',
+					},
+				],
+			},
+		],
+		displayOptions: {
+			show: {
+				resource: ['trust'],
+				operation: ['setTrustline'],
+			},
+		},
+	},
+	{
+		displayName: 'Authorized',
+		name: 'authorized',
+		type: 'boolean',
+		displayOptions: {
+			show: {
+				resource: ['trust'],
+				operation: ['setTrustline'],
+			},
+		},
+		default: true,
+		description: 'Whether the trustor is authorized to transact with the asset or not',
+	},
+	{
+		displayName: 'Authorized To Maintain Liabilities',
+		name: 'authorizedToMaintainLiabilities',
+		type: 'boolean',
+		displayOptions: {
+			show: {
+				resource: ['trust'],
+				operation: ['setTrustline'],
+			},
+		},
+		default: false,
+		description:
+			'Whether he trustor is authorized to maintain offers but not to perform other transactions',
+	},
+	{
+		displayName: 'Clawback Enabled',
+		name: 'clawbackEnabled',
+		type: 'boolean',
+		displayOptions: {
+			show: {
+				resource: ['trust'],
+				operation: ['setTrustline'],
+			},
+		},
+		default: true,
+		description:
+			'Whether the issuer will be allowed to clawback its asset (both from accounts and claimable balances)',
+	},
+];
---
+++ nodes/Soroban/actions/trust/setTrustline/execute.ts
@@ -0,0 +1,33 @@
+import { IExecuteFunctions } from 'n8n-workflow';
+import { Operation, Asset } from 'soroban-client';
+import IAsset from '../../../../../common/interfaces/stellar/IAsset';
+import { StellarPlatformEnum } from '../../../../../common/enum/stellar/StellarPlatformEnum';
+import { buildAsset } from '../../../../../common/utils/stellar/buildAsset';
+
+export async function setTrustline(this: IExecuteFunctions) {
+	try {
+		const trustor = this.getNodeParameter('trustor', 0) as string;
+		const { values: assetToTrust } = this.getNodeParameter('trustAsset', 0) as IAsset;
+		const authorized = this.getNodeParameter('authorized', 0) as boolean;
+		const authorizedToMaintainLiabilities = this.getNodeParameter(
+			'authorizedToMaintainLiabilities',
+			0,
+		) as boolean;
+		const clawbackEnabled = this.getNodeParameter('clawbackEnabled', 0) as boolean;
+
+		const asset = buildAsset(assetToTrust, StellarPlatformEnum.SOROBAN) as Asset;
+
+		const setTrustlineOperation = Operation.setTrustLineFlags({
+			trustor,
+			asset,
+			flags: {
+				authorized,
+				authorizedToMaintainLiabilities,
+				clawbackEnabled,
+			},
+		}).toXDR('base64');
+		return { operation: setTrustlineOperation };
+	} catch (error) {
+		throw new Error(error);
+	}
+}
