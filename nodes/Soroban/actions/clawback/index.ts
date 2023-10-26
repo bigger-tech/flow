d1bfe55b217a703c8769ba5a029beb398d0ed63f
Andres Nazzari <lordcliff@gmail.com>
2023-10-27 00:55:33 +0900

    Create clawback description

---
+++ nodes/Soroban/actions/clawback/index.ts
@@ -0,0 +1,5 @@
+import * as clawback from './clawback';
+import * as clawbackClaimableBalance from './clawbackClaimableBalance';
+import description from './description';
+
+export { clawback, clawbackClaimableBalance, description };
