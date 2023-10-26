406b6be66c33c669e4178c1359cd00daec5bc32e
Gonzalo Altamirano <55992031+GonzaloAlt@users.noreply.github.com>
2023-10-26 16:14:41 -0300

    Create beginSponsoring soroban node

---
+++ nodes/Soroban/actions/sponsorship/beginSponsoring/execute.ts
@@ -0,0 +1,15 @@
+import { IExecuteFunctions } from 'n8n-workflow';
+import { Operation } from 'soroban-client';
+
+export async function beginSponsoring(this: IExecuteFunctions) {
+	try {
+		const sponsoredId = this.getNodeParameter('sponsoredId', 0) as string;
+		const beginSponsoringOperation = Operation.beginSponsoringFutureReserves({ sponsoredId }).toXDR(
+			'base64',
+		);
+
+		return { operation: beginSponsoringOperation };
+	} catch (error) {
+		throw new Error(error);
+	}
+}
