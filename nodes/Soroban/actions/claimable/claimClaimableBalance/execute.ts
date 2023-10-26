327bea33c58941da1f90a3185cfd070f39e42667
Andres Nazzari <lordcliff@gmail.com>
2023-10-27 03:48:29 +0900

    Add transaction field to get claimableId if not provided

--- nodes/Soroban/actions/claimable/claimClaimableBalance/execute.ts
+++ nodes/Soroban/actions/claimable/claimClaimableBalance/execute.ts
@@ -1,12 +1,24 @@
 import { IExecuteFunctions } from 'n8n-workflow';
-import { Operation } from 'soroban-client';
+import { Operation, Transaction } from 'soroban-client';
 
 export async function claimClaimableBalance(this: IExecuteFunctions) {
 	try {
-		const balanceId = this.getNodeParameter('balanceId', 0) as string;
+		let balanceId = this.getNodeParameter('balanceId', 0) as string;
+		const transaction = this.getNodeParameter('transaction', 0) as string;
+
+		if (!transaction && !balanceId) {
+			throw new Error('Either transaction or balanceId must be provided');
+		}
+
+		if (!balanceId) {
+			const txResult = new Transaction(transaction, 'base64');
+			balanceId = txResult.getClaimableBalanceId(0);
+		}
+
 		const claimClaimableBalanceOperation = Operation.claimClaimableBalance({
 			balanceId,
 		}).toXDR('base64');
+
 		return { operation: claimClaimableBalanceOperation };
 	} catch (error) {
 		throw new Error(error);
