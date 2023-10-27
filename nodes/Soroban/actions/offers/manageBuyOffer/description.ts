85de66f9b29fdb3648f6df279f63f86d663c4d71
Andres Nazzari <lordcliff@gmail.com>
2023-10-27 22:26:18 +0900

    Create manageBuyOffer action in soroban node

---
+++ nodes/Soroban/actions/offers/manageBuyOffer/description.ts
@@ -0,0 +1,181 @@
+import { INodeProperties } from 'n8n-workflow';
+
+export const manageBuyOfferDescription: INodeProperties[] = [
+	{
+		displayName: 'Selling Asset',
+		name: 'sellingAsset',
+		type: 'fixedCollection',
+		default: {},
+		placeholder: 'Select asset',
+		required: true,
+		typeOptions: {
+			multipleValues: false,
+		},
+		options: [
+			{
+				name: 'values',
+				displayName: 'Asset',
+				values: [
+					{
+						displayName: 'Asset Type',
+						name: 'isNative',
+						type: 'options',
+						required: true,
+						options: [
+							{
+								name: 'Native',
+								value: true,
+							},
+							{
+								name: 'Custom Asset',
+								value: false,
+							},
+						],
+						default: true,
+					},
+					{
+						displayName: 'Code',
+						name: 'code',
+						type: 'string',
+						default: '',
+						required: true,
+						displayOptions: {
+							show: {
+								isNative: [false],
+							},
+						},
+					},
+					{
+						displayName: 'Issuer',
+						name: 'issuer',
+						type: 'string',
+						default: '',
+						required: true,
+						placeholder: 'GCEVJ...',
+						displayOptions: {
+							show: {
+								isNative: [false],
+							},
+						},
+					},
+				],
+			},
+		],
+		displayOptions: {
+			show: {
+				resource: ['offers'],
+				operation: ['manageBuyOffer'],
+			},
+		},
+		description: 'Asset the offer creator is selling',
+	},
+	{
+		displayName: 'Buying Asset',
+		name: 'buyingAsset',
+		type: 'fixedCollection',
+		default: {},
+		required: true,
+		placeholder: 'Select asset',
+		typeOptions: {
+			multipleValues: false,
+		},
+		options: [
+			{
+				name: 'values',
+				displayName: 'Asset',
+				values: [
+					{
+						displayName: 'Asset Type',
+						name: 'isNative',
+						type: 'options',
+						required: true,
+
+						options: [
+							{
+								name: 'Native',
+								value: true,
+							},
+							{
+								name: 'Custom Asset',
+								value: false,
+							},
+						],
+						default: true,
+					},
+					{
+						displayName: 'Code',
+						name: 'code',
+						type: 'string',
+						default: '',
+						displayOptions: {
+							show: {
+								isNative: [false],
+							},
+						},
+					},
+					{
+						displayName: 'Issuer',
+						name: 'issuer',
+						type: 'string',
+						default: '',
+						placeholder: 'GCEVJ...',
+						displayOptions: {
+							show: {
+								isNative: [false],
+							},
+						},
+					},
+				],
+			},
+		],
+		displayOptions: {
+			show: {
+				resource: ['offers'],
+				operation: ['manageBuyOffer'],
+			},
+		},
+		description: 'Asset the offer creator is buying',
+	},
+	{
+		displayName: 'Amount You Are Buying',
+		name: 'buyingAmount',
+		type: 'number',
+		required: true,
+		displayOptions: {
+			show: {
+				resource: ['offers'],
+				operation: ['manageBuyOffer'],
+			},
+		},
+		default: '',
+		description: 'An amount of zero will delete the offer',
+	},
+	{
+		displayName: 'Price of 1 Unit of Buying in Terms of Selling',
+		name: 'price',
+		type: 'number',
+		required: true,
+		displayOptions: {
+			show: {
+				resource: ['offers'],
+				operation: ['manageBuyOffer'],
+			},
+		},
+		default: '',
+	},
+	{
+		displayName: 'Offer ID',
+		name: 'offerId',
+		type: 'string',
+		required: true,
+		displayOptions: {
+			show: {
+				resource: ['offers'],
+				operation: ['manageBuyOffer'],
+			},
+		},
+		description:
+			'If 0, will create a new offer. Existing offer ID numbers can be found using the Offers for Account endpoint.',
+		default: '',
+	},
+];
---
+++ nodes/Soroban/actions/offers/manageBuyOffer/execute.ts
@@ -0,0 +1,33 @@
+import { IExecuteFunctions } from 'n8n-workflow';
+import { Operation, Asset } from 'soroban-client';
+import { convertAmountToBigNumber } from '../../../../../common/utils/stellar/convertAmountToBigNumber';
+import IAsset from '../../../../../common/interfaces/stellar/IAsset';
+import { buildAsset } from '../../../../../common/utils/stellar/buildAsset';
+import { StellarPlatformEnum } from '../../../../../common/enum/stellar/StellarPlatformEnum';
+
+export async function manageBuyOffer(this: IExecuteFunctions) {
+	try {
+		const { values: sellingAsset } = this.getNodeParameter('sellingAsset', 0) as IAsset;
+		const { values: buyingAsset } = this.getNodeParameter('buyingAsset', 0) as IAsset;
+		const sellingAmount = this.getNodeParameter('buyingAmount', 0) as number;
+		const sellingPrice = this.getNodeParameter('price', 0) as number;
+		const offerId = this.getNodeParameter('offerId', 0) as string;
+
+		const selling = buildAsset(sellingAsset, StellarPlatformEnum.SOROBAN) as Asset;
+		const buying = buildAsset(buyingAsset, StellarPlatformEnum.SOROBAN) as Asset;
+		const buyAmount = convertAmountToBigNumber(sellingAmount);
+		const price = convertAmountToBigNumber(sellingPrice);
+
+		const manageBuyOfferOperation = Operation.manageBuyOffer({
+			selling,
+			buying,
+			buyAmount,
+			price,
+			offerId,
+		}).toXDR('base64');
+
+		return { operation: manageBuyOfferOperation };
+	} catch (error) {
+		throw new Error(error);
+	}
+}
