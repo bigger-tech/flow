7b7a9734b389e97316a41f64f38f0d28bfea990b
Andres Nazzari <lordcliff@gmail.com>
2023-10-31 05:54:59 +0900

    Create findLiquidityPool description

---
+++ nodes/Soroban/actions/server/description.ts
@@ -0,0 +1,33 @@
+import { INodeProperties } from 'n8n-workflow';
+import * as findLiquidityPool from './findLiquidityPool';
+const description: INodeProperties[] = [
+	{
+		displayName: 'Operation',
+		name: 'operation',
+		type: 'options',
+		default: 'findLiquidityPool',
+		noDataExpression: true,
+		displayOptions: {
+			show: {
+				resource: ['server'],
+			},
+		},
+		options: [
+			{
+				name: 'Find Liquidity Pool',
+				value: 'findLiquidityPool',
+				description: 'Find liquidity pools availables',
+				action: 'Find liquidity pools availables',
+			},
+			{
+				name: 'Find Payments',
+				value: 'findPayments',
+				description: 'Find payments received by an account',
+				action: 'Find payments',
+			},
+		],
+	},
+	...findLiquidityPool.description,
+];
+
+export default description;
