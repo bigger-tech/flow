6f444ab5c030e9adceda973f770222b1ae6e7f21
GonzaloAlt <55992031+GonzaloAlt@users.noreply.github.com>
2023-11-03 18:04:37 -0300

    Add space in transactions index for neatness
    Co-authored-by: Mauricio Genebrieres <mauryavero@gmail.com>

--- nodes/Mykobo/actions/transactions/index.ts
+++ nodes/Mykobo/actions/transactions/index.ts
@@ -3,4 +3,5 @@ import { execute as deposit } from './deposit';
 import { execute as withdraw } from './withdraw';
 import { execute as depositInteractive } from './depositInteractive';
 import { execute as withdrawInteractive } from './withdrawInteractive';
+
 export { deposit, withdraw, depositInteractive, withdrawInteractive, description };
