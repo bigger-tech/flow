7b6920c0344ac8de5690b7117d41e03c3c944b4b
GonzaloAlt <55992031+GonzaloAlt@users.noreply.github.com>
2023-11-03 18:03:42 -0300

    Add spaces in token validate execute for neatness
    Co-authored-by: Mauricio Genebrieres <mauryavero@gmail.com>

--- nodes/Mykobo/actions/token/validate/execute.ts
+++ nodes/Mykobo/actions/token/validate/execute.ts
@@ -4,7 +4,9 @@ import SEP10 from '../../../../../common/repository/anchor/SEP10';
 
 export async function validate(this: IExecuteFunctions) {
 	const validateXdr = this.getNodeParameter('validateXdr', 0) as string;
+	
 	const mykoboCredentials = new AnchorCredentials(await this.getCredentials('mykoboApi'));
 	const sep10 = new SEP10(mykoboCredentials);
+	
 	return await sep10.validateChallenge(validateXdr);
 }
