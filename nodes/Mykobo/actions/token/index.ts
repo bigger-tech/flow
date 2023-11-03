bb00d3771e2bcead2be4edf8b8d5720f74bb5f79
GonzaloAlt <55992031+GonzaloAlt@users.noreply.github.com>
2023-11-03 18:01:59 -0300

    Add space in mykobo token index
    Co-authored-by: Mauricio Genebrieres <mauryavero@gmail.com>

--- nodes/Mykobo/actions/token/index.ts
+++ nodes/Mykobo/actions/token/index.ts
@@ -3,4 +3,5 @@ import { execute as send } from './send';
 import { execute as validate } from './validate';
 import { execute as sign } from './sign';
 import description from './description';
+
 export { get, send, validate, sign, description };
