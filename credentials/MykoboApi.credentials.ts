0dd24846cecc6b92b0e6ed3ab3f82e8e97a3aff6
Gonzalo Altamirano <55992031+GonzaloAlt@users.noreply.github.com>
2023-11-03 17:36:24 -0300

    Add anchorName property in MykoboApi.credentials

--- credentials/MykoboApi.credentials.ts
+++ credentials/MykoboApi.credentials.ts
@@ -39,5 +39,12 @@ export class MykoboApi implements ICredentialType {
 				password: true,
 			},
 		},
+		{
+			displayName: 'Anchor Name',
+			required: true,
+			name: 'anchorName',
+			type: 'hidden',
+			default: 'mykobo',
+		},
 	];
 }
