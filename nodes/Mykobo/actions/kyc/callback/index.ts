bfa277ad04e43629243ddb6d5952f66888f2df95
GonzaloAlt <55992031+GonzaloAlt@users.noreply.github.com>
2023-11-03 18:35:00 -0300

    fix: delete not implemented KYC endpoint(#63)

--- nodes/Mykobo/actions/kyc/callback/index.ts
+++
@@ -1,4 +0,0 @@
-import { callbackDescription as description } from './description';
-import { setCallbackUrl as execute } from './execute';
-
-export { description, execute };
--- nodes/Mykobo/actions/kyc/index.ts
+++ nodes/Mykobo/actions/kyc/index.ts
@@ -1,6 +1,5 @@
 import { execute as status } from './status';
-import { execute as callback } from './callback';
 import { execute as deleteKYCInformation } from './delete';
 import description from './description';
 
-export { status, callback, deleteKYCInformation, description };
+export { status, deleteKYCInformation, description };
