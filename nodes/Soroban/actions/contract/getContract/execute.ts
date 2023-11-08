4757651cdd6548df946df461aa908d0d61178170
Andres Nazzari <lordcliff@gmail.com>
2023-11-08 22:22:06 +0900

    Change function name

--- nodes/Soroban/actions/contract/getContract/execute.ts
+++ nodes/Soroban/actions/contract/getContract/execute.ts
@@ -1,11 +1,11 @@
 import { IExecuteFunctions } from 'n8n-workflow';
-import { getContractAbi, transformABI } from './helpers/helpers';
+import { getContractAbi, transformAbi } from './helpers/helpers';
 
 export async function getContract(this: IExecuteFunctions) {
 	const contractId = this.getNodeParameter('contractId', 0) as string;
 
 	const methods = await getContractAbi(contractId);
-	const methodsTransformed = transformABI(methods);
+	const methodsTransformed = transformAbi(methods);
 
 	return { methods: methodsTransformed };
 }
