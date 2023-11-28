bc9f849f35e40b465b589c33255c08c6f3931dd4
Gonzalo Altamirano <55992031+GonzaloAlt@users.noreply.github.com>
2023-11-28 06:35:55 -0300

    Update invoke execute file to use contract.call

--- nodes/Soroban/actions/contract/invokeContract/execute.ts
+++ nodes/Soroban/actions/contract/invokeContract/execute.ts
@@ -1,31 +1,28 @@
 import { IExecuteFunctions } from 'n8n-workflow';
-import { xdr, Operation, StrKey, Address } from 'soroban-client';
-import { hexToByte } from './helpers/functions';
+import { Contract, xdr } from 'soroban-client';
+import { CustomScVal, toScVal } from './helpers/helpers';
 
 export async function invokeContract(this: IExecuteFunctions) {
+	const functionName = this.getNodeParameter('functionName', 0) as string;
+	const contractAddress = this.getNodeParameter('contractAddress', 0) as string;
+	const { contractParams }: { contractParams: CustomScVal[] } = this.getNodeParameter(
+		'functionParams',
+		0,
+	) as { contractParams: CustomScVal[] };
+
+	const params: xdr.ScVal[] = [];
+	if (contractParams) {
+		for (const { value, type } of contractParams) {
+			params.push(toScVal(value, type));
+		}
+	}
+
 	try {
-		const functionName = this.getNodeParameter('functionName', 0) as string;
-		const contractId = this.getNodeParameter('contractId', 0) as string;
-		const sourceAccount = this.getNodeParameter('sourceAccount', 0) as string;
+		const contract = new Contract(contractAddress);
 
-		const byteContractId = hexToByte(contractId);
-		const contractAdress = StrKey.encodeContract(byteContractId);
-		const address = new Address(contractAdress);
-		const invokeContract = new xdr.InvokeContractArgs({
-			contractAddress: address.toScAddress(),
-			functionName: functionName,
-			args: [],
-		});
+		const contractOperation = contract.call(functionName, ...params).toXDR('base64');
 
-		const hostFunction: xdr.HostFunction =
-			xdr.HostFunction.hostFunctionTypeInvokeContract(invokeContract);
-
-		const operationEnvelope = Operation.invokeHostFunction({
-			func: hostFunction,
-			auth: [],
-		});
-
-		return { operation: operationEnvelope.toXDR('base64'), sourceAccount };
+		return { operation: contractOperation };
 	} catch (error) {
 		throw new Error(error);
 	}
